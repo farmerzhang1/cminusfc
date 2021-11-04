8a0b6e169ac506fe762acd0edd3fbe85c77ebd6c
Jiahao Li <lijh2015@mail.ustc.edu.cn>
2021-11-04 19:30:07 +0800

    Update cminusf_builder.cpp

--- src/cminusfc/cminusf_builder.cpp
+++ src/cminusfc/cminusf_builder.cpp
@@ -3,8 +3,8 @@
 // use these macros to get constant value
 #define CONST_FP(num) \
     ConstantFP::get((float)num, module.get())
-#define CONST_ZERO(type) \
-    ConstantZero::get(var_type, module.get())
+#define CONST_INT(num) \
+    ConstantInt::get(num, module.get())
 
 
 // You can define global variables here
