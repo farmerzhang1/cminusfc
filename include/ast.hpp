d21ae12b43724192b65a1a1a6fcbeb18b242cb5b
farmerzhang1 <farmerzhang1@mail.ustc.edu.cn>
2021-11-27 15:36:31 +0800

    good old days

--- include/ast.hpp
+++ include/ast.hpp
@@ -1,20 +1,25 @@
 #ifndef _SYNTAX_TREE_HPP_
 #define _SYNTAX_TREE_HPP_
-extern "C" {
+extern "C"
+{
 #include "syntax_tree.h"
     extern syntax_tree *parse(const char *input);
 }
 #include <vector>
 #include <memory>
 #include <string>
+#include <map>
+#include <functional>
 
-enum class CminusType {
+enum class CminusType
+{
     TYPE_INT,
     TYPE_FLOAT,
     TYPE_VOID
 };
 
-enum class RelOp {
+enum class RelOp
+{
     // <=
     OP_LE,
     // <
@@ -29,14 +34,16 @@ enum class RelOp {
     OP_NEQ
 };
 
-enum class AddOp {
+enum class AddOp
+{
     // +
     OP_PLUS,
     // -
     OP_MINUS
 };
 
-enum class MulOp {
+enum class MulOp
+{
     // *
     OP_MUL,
     // /
@@ -64,86 +71,163 @@ struct ASTVar;
 struct ASTAssignExpression;
 struct ASTSimpleExpression;
 struct ASTAdditiveExpression;
-struct ASTTerm ;
+struct ASTTerm;
 struct ASTCall;
 
 class ASTVisitor;
 
-class AST {
+class AST
+{
 public:
     AST() = delete;
     AST(syntax_tree *);
-    AST(AST &&tree) {
+    AST(AST &&tree)
+    {
         root = tree.root;
         tree.root = nullptr;
     };
-    ASTProgram* get_root() { return root.get(); }
-    void run_visitor(ASTVisitor& visitor);
+    ASTProgram *get_root() { return root.get(); }
+    void run_visitor(ASTVisitor &visitor);
+    std::map<AddOp, std::function<int(int, int)>> add_int_map = {
+        {AddOp::OP_PLUS, [](int l, int r)
+         { return l + r; }},
+        {AddOp::OP_MINUS, [](int l, int r)
+         { return l - r; }}};
+    std::map<AddOp, std::function<float(float, float)>> add_float_map = {
+        {AddOp::OP_PLUS, [](float l, float r)
+         { return l + r; }},
+        {AddOp::OP_MINUS, [](float l, float r)
+         { return l - r; }}};
+    std::map<MulOp, std::function<int(int, int)>> mul_int_map = {
+        {MulOp::OP_MUL, [](int l, int r)
+         { return l * r; }},
+        {MulOp::OP_DIV, [](int l, int r)
+         { return l / r; }}};
+    std::map<MulOp, std::function<float(float, float)>> mul_float_map = {
+        {MulOp::OP_MUL, [](float l, float r)
+         { return l * r; }},
+        {MulOp::OP_DIV, [](float l, float r)
+         { return l / r; }}};
+    std::map<RelOp, std::function<bool(int, int)>> comp_int_map = {
+        {RelOp::OP_EQ, [](int l, int r)
+         { return l == r; }},
+        {RelOp::OP_NEQ, [](int l, int r)
+         { return l != r; }},
+        {RelOp::OP_GE, [](int l, int r)
+         { return l >= r; }},
+        {RelOp::OP_GT, [](int l, int r)
+         { return l > r; }},
+        {RelOp::OP_LE, [](int l, int r)
+         { return l <= r; }},
+        {RelOp::OP_LT, [](int l, int r)
+         { return l < r; }}};
+    std::map<RelOp, std::function<bool(float, float)>> comp_float_map = {
+        {RelOp::OP_EQ, [](float l, float r)
+         { return l == r; }},
+        {RelOp::OP_NEQ, [](float l, float r)
+         { return l != r; }},
+        {RelOp::OP_GE, [](float l, float r)
+         { return l >= r; }},
+        {RelOp::OP_GT, [](float l, float r)
+         { return l > r; }},
+        {RelOp::OP_LE, [](float l, float r)
+         { return l <= r; }},
+        {RelOp::OP_LT, [](float l, float r)
+         { return l < r; }}};
+    // we assume pointers are non-empty
+    bool both_int(ASTNum *l, ASTNum *r);
+    float to_f(ASTNum *n);
+
 private:
-    ASTNode* transform_node_iter(syntax_tree_node *);
+    ASTNode *transform_node_iter(syntax_tree_node *);
     std::shared_ptr<ASTProgram> root = nullptr;
 };
 
-struct ASTNode {
+struct ASTNode
+{
     virtual void accept(ASTVisitor &) = 0;
+    // we should call it: connect all the way down to an ASTNum
+    virtual bool is_num() = 0;
 };
 
-struct ASTProgram : ASTNode {
+struct ASTProgram : ASTNode
+{
     virtual void accept(ASTVisitor &) override final;
     std::vector<std::shared_ptr<ASTDeclaration>>
         declarations;
+    bool is_num() override final { return false; }
 };
 
-struct ASTDeclaration: ASTNode {
+struct ASTDeclaration : ASTNode
+{
     CminusType type;
     std::string id; // 这里是数字 int or float
     virtual ~ASTDeclaration() {}
+    bool is_num() override final { return false; }
 };
 
-struct ASTFactor: ASTNode { virtual ~ASTFactor(){} };
+struct ASTFactor : ASTNode
+{
+    virtual ~ASTFactor() {}
+    bool is_num() override;
+};
 
-struct ASTNum: ASTFactor {
+struct ASTNum : ASTFactor
+{
     virtual void accept(ASTVisitor &) override final;
     CminusType type;
-    union {
+    union
+    {
         int i_val;
         float f_val;
     };
+    bool is_num() override final { return true; }
 };
 
-struct ASTVarDeclaration: ASTDeclaration {
+struct ASTVarDeclaration : ASTDeclaration
+{
     virtual void accept(ASTVisitor &) override final;
     std::shared_ptr<ASTNum> num; // num若不为空，值为数组类型和数组个数
 };
 
-struct ASTFunDeclaration: ASTDeclaration {
+struct ASTFunDeclaration : ASTDeclaration
+{
     virtual void accept(ASTVisitor &) override final;
     std::vector<std::shared_ptr<ASTParam>> params;
     std::shared_ptr<ASTCompoundStmt> compound_stmt;
 };
 
-struct ASTParam: ASTNode {
+struct ASTParam : ASTNode
+{
     virtual void accept(ASTVisitor &) override final;
     CminusType type;
     std::string id;
     // true if it is array param
     bool isarray;
+    bool is_num() override final { return false; }
 };
 
-struct ASTStatement : ASTNode { virtual ~ASTStatement() {} };
+struct ASTStatement : ASTNode
+{
+    virtual ~ASTStatement() {}
+    bool is_num() override final { return false; }
+};
 
-struct ASTCompoundStmt: ASTStatement {
-    virtual void accept(ASTVisitor&) override final;
+struct ASTCompoundStmt : ASTStatement
+{
+    virtual void accept(ASTVisitor &) override final;
     std::vector<std::shared_ptr<ASTVarDeclaration>> local_declarations;
     std::vector<std::shared_ptr<ASTStatement>> statement_list;
 };
 
-struct ASTExpressionStmt: ASTStatement {
+struct ASTExpressionStmt : ASTStatement
+{
     virtual void accept(ASTVisitor &) override final;
     std::shared_ptr<ASTExpression> expression;
 };
 
-struct ASTSelectionStmt: ASTStatement {
+struct ASTSelectionStmt : ASTStatement
+{
     virtual void accept(ASTVisitor &) override final;
     std::shared_ptr<ASTExpression> expression;
     std::shared_ptr<ASTStatement> if_statement;
@@ -151,61 +235,77 @@ struct ASTSelectionStmt: ASTStatement {
     std::shared_ptr<ASTStatement> else_statement;
 };
 
-struct ASTIterationStmt: ASTStatement {
+struct ASTIterationStmt : ASTStatement
+{
     virtual void accept(ASTVisitor &) override final;
     std::shared_ptr<ASTExpression> expression;
     std::shared_ptr<ASTStatement> statement;
 };
 
-struct ASTReturnStmt: ASTStatement {
+struct ASTReturnStmt : ASTStatement
+{
     virtual void accept(ASTVisitor &) override final;
     // should be nullptr if return void
     std::shared_ptr<ASTExpression> expression;
 };
 
-struct ASTExpression: ASTFactor { };
+struct ASTExpression : ASTFactor
+{
+    bool is_num() override;
+};
 
-struct ASTAssignExpression: ASTExpression {
+struct ASTAssignExpression : ASTExpression
+{
     virtual void accept(ASTVisitor &) override final;
     std::shared_ptr<ASTVar> var;
     std::shared_ptr<ASTExpression> expression;
+    bool is_num() override final { return false; }
 };
 
-struct ASTSimpleExpression: ASTExpression {
+struct ASTSimpleExpression : ASTExpression
+{
     virtual void accept(ASTVisitor &) override final;
     std::shared_ptr<ASTAdditiveExpression> additive_expression_l; // lhs is always nonempty
     std::shared_ptr<ASTAdditiveExpression> additive_expression_r;
     RelOp op;
+    bool is_num() override final;
 };
 
-struct ASTVar: ASTFactor {
+struct ASTVar : ASTFactor
+{
     virtual void accept(ASTVisitor &) override final;
     std::string id;
     // nullptr if var is of int type
     std::shared_ptr<ASTExpression> expression;
 };
 
-struct ASTAdditiveExpression: ASTNode {
+struct ASTAdditiveExpression : ASTNode
+{
     virtual void accept(ASTVisitor &) override final;
     std::shared_ptr<ASTAdditiveExpression> additive_expression;
     AddOp op;
     std::shared_ptr<ASTTerm> term;
+    bool is_num() override final;
 };
 
-struct ASTTerm : ASTNode {
+struct ASTTerm : ASTNode
+{
     virtual void accept(ASTVisitor &) override final;
     std::shared_ptr<ASTTerm> term;
     MulOp op;
     std::shared_ptr<ASTFactor> factor;
+    bool is_num() override final;
 };
 
-struct ASTCall: ASTFactor {
+struct ASTCall : ASTFactor
+{
     virtual void accept(ASTVisitor &) override final;
     std::string id;
     std::vector<std::shared_ptr<ASTExpression>> args;
 };
 
-class ASTVisitor {
+class ASTVisitor
+{
 public:
     virtual void visit(ASTProgram &) = 0;
     virtual void visit(ASTNum &) = 0;
@@ -225,7 +325,8 @@ public:
     virtual void visit(ASTCall &) = 0;
 };
 
-class ASTPrinter : public ASTVisitor {
+class ASTPrinter : public ASTVisitor
+{
 public:
     virtual void visit(ASTProgram &) override final;
     virtual void visit(ASTNum &) override final;
@@ -245,8 +346,9 @@ public:
     virtual void visit(ASTCall &) override final;
     void add_depth() { depth += 2; }
     void remove_depth() { depth -= 2; }
+
 private:
     int depth = 0;
 };
-#endif
 
+#endif
