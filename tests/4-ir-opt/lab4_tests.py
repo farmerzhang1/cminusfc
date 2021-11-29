82f986d0bd7ba26d4a8a912e550536eb0031a7b4
farmerzhang1 <farmerzhang1@mail.ustc.edu.cn>
2021-11-29 14:44:00 +0800

    update baseline command

--- tests/4-ir-opt/lab4_tests.py
+++ tests/4-ir-opt/lab4_tests.py
@@ -39,7 +39,8 @@ def compile_baseline_files(file_lists):
     exec_files = list()
     for each in file_lists:
         exec_file,_=os.path.splitext(each)
-        COMMAND = "clang -O0 -w " + each + " -o " + exec_file + " -L. -lcminus_io"
+        COMMAND = "clang -O0 -w " + each + " -o " + exec_file + " -L ../../build/ -lcminus_io"
+        # print(COMMAND)
         try:
             result = subprocess.run(COMMAND, stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True,timeout=1)
             if result.returncode == 0:
